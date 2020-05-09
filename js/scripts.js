627d653e120f945f5751e6fa043e861307481ab7
Carmen Chapa <cmchapa28@gmail.com>
2020-05-09 18:06:07 +0200

    style(): use media query for mobile bar instead

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-// We listen to the resize event
-window.addEventListener("resize", () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty("--vh", `${vh}px`)
-})
